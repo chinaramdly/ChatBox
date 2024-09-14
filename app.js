18cfdf457ecd0d395a210048e4d16fdf88840b4e
chinara mammadova <cmmmdova739@gmail.com>
2024-09-14 12:42:26 +0000

    send

--- app.js
+++ app.js
@@ -26,6 +26,32 @@ $(document).ready(function () {
 
     });
 
+    
+
+    $('#you').click(function (e) {
+        e.preventDefault();
+        function myel(myclass = "") {
+            if (!$.trim($("#inputyou").val()) == "") {
+                let vall = $("#inputyou").val()
+                    , newH3 = $('<h3> </h3>')
+                    , newDiv = $('<div class="line"> </div>')
+                    , bigDiv = $(`<div ${myclass}> </div>`)
+
+                $(newH3).append(vall)
+                $(newDiv).append(newH3)
+                $(bigDiv).append(newDiv)
+                return bigDiv
+            } else {
+                return
+            }
+
+        }
+        $('#messageme').append(myel())
+        $('#messageyou').append(myel('class="end"'))
+        $("#inputyou").val(" ");
+
+
+    });
 
 
 
