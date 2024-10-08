66276837dec5b73b6767c68050e47a4df1272047
chinara mammadova <cmmmdova739@gmail.com>
2024-10-08 12:33:32 +0000

    scroll

--- app.js
+++ app.js
@@ -3,55 +3,50 @@ $(document).ready(function () {
 
     $("#me").click(function (e) {
         e.preventDefault();
-        function myel(myclass ="") {
-            if (!$.trim($("#inputme").val()) == "") {
+        function myel(myclass = " ") {
+            if (!$.trim($("#inputme").val()) == '') {
                 let vall = $("#inputme").val()
                     , newH3 = $('<h3> </h3>')
                     , newDiv = $('<div class="line"> </div>')
                     , bigDiv = $(`<div ${myclass}> </div>`)
-
                 $(newH3).append(vall)
                 $(newDiv).append(newH3)
                 $(bigDiv).append(newDiv)
                 return bigDiv
-            } else {
+            }
+            else {
                 return
             }
-
         }
         $("#messageyou").append(myel())
         $("#messageme").append(myel('class="end"'))
-        $("#inputme").val("");
-        document.querySelector("#messageme").scrollTo(0, document.querySelector("#messageme").scrollHeight)
-        document.querySelector("#messageyou").scrollTo(0, document.querySelector("#messageyou").scrollHeight)
+        $("#inputme").val(" ")
+        document.querySelector("#messageme").scrollTo(0, document.querySelector('#messageme').scrollHeight)
+        document.querySelector("#messageyou").scrollTo(0, document.querySelector('#messageyou').scrollHeight)
     });
-
-
 
     $("#you").click(function (e) {
         e.preventDefault();
-        function myel(myclass = "") {
-            if (!$.trim($("#inputyou").val()) == "") {
+        function myel(myclass = " ") {
+            if (!$.trim($("#inputyou").val())== ' ') {
                 let vall = $("#inputyou").val()
                     , newH3 = $('<h3> </h3>')
                     , newDiv = $('<div class="line"> </div>')
                     , bigDiv = $(`<div ${myclass}> </div>`)
-
                 $(newH3).append(vall)
                 $(newDiv).append(newH3)
                 $(bigDiv).append(newDiv)
                 return bigDiv
-            } else {
+            }
+            else {
                 return
             }
-
         }
         $("#messageme").append(myel())
         $("#messageyou").append(myel('class="end"'))
-        $("#inputyou").val("");
-        document.querySelector("#messageme").scrollTo(0, document.querySelector("#messageme").scrollWidth)
-        document.querySelector("#messageyou").scrollTo(0, document.querySelector("#messageyou").scrollWidth)
-
+        $("#inputyou").val(" ")
+        document.querySelector('#messageme').scrollTo(0,document.querySelector('#messageme').scrollHeight)
+        document.querySelector('#messageyou').scrollTo(0,document.querySelector('#messageyou').scrollHeight)
     });
 
 
@@ -62,4 +57,5 @@ $(document).ready(function () {
 
 
 
- });
+
+});
